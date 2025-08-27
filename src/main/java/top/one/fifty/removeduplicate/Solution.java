b889939c197eca757dc1127a9f2b390c7b697e45
anwar mulla <007anwarmulla@gmail.com>
2025-08-27 10:55:12 +0530

    leetcode 80: removeDuplicates

--- src/main/java/top/one/fifty/removeduplicate/Solution.java
+++ src/main/java/top/one/fifty/removeduplicate/Solution.java
@@ -1,7 +1,22 @@
 package top.one.fifty.removeduplicate;
 
 class Solution {
+    public static void main(String[] args) {
+        new Solution().removeDuplicates(new int[]{0,0,1,1,1,1,2,3,3});
+    }
     public int removeDuplicates(int[] nums) {
-        
+        int i=0,min=0,length=nums.length;
+        while(i+2<length)
+        {
+            if(nums[i]==nums[i+1] && nums[i+1]==nums[i+2]) {
+                min++;
+                for (int j = i + 2; j < length - 1; j++)
+                    nums[j] = nums[j + 1];
+                length--;
+            }else{
+            i++;
+            }
+        }
+        return (nums.length-min);
     }
 }
